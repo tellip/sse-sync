f11acb64f748cbd9d2b6099e6ee6d656403be0e4
tellip <luojunyi.0@gmail.com>
2026-05-17 17:55:07 +0800

    asdf

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,25 @@
-const createConfig = (input, outputDir, additionalExternals = []) => ({
+import resolve from '@rollup/plugin-node-resolve';
+
+const createConfig = (input, outputDir, externals = []) => ({
     input,
     output: [
-        {file: `${outputDir}/index.js`, format: 'esm', sourcemap: true},
-        {file: `${outputDir}/index.cjs`, format: 'cjs', sourcemap: true, exports: 'named'},
+        {
+            file: `${outputDir}/index.js`,
+            format: 'esm',
+            sourcemap: true,
+        },
+        {
+            file: `${outputDir}/index.cjs`,
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'named',
+        },
     ],
-    external: ['koa', 'assert', ...additionalExternals],  // 添加 Node 内置模块
+    external: externals,
+    plugins: [resolve()],
 });
 
 export default [
-    createConfig('src/server/index.js', 'dist/server'),
-    createConfig('src/client/index.js', 'dist/client'),
+    createConfig('src/server/index.js', 'dist/server', ['koa', 'assert']),
+    createConfig('src/client/index.js', 'dist/client', []),
 ];
